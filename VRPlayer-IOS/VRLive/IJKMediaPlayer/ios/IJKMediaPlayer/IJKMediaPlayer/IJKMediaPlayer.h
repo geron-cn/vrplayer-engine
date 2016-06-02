148719093f033494c37d52283571447a3eef5a0e
super626 <273068367@qq.com>
2016-06-02 15:34:04 +0800

    add ijkplayer

---
+++ VRPlayer-IOS/VRLive/IJKMediaPlayer/ios/IJKMediaPlayer/IJKMediaPlayer/IJKMediaPlayer.h
@@ -0,0 +1,13 @@
+//
+//  IJKMediaPlayer.h
+//  IJKMediaPlayer
+//
+//  Created by yangxiao on 16/6/1.
+//  Copyright © 2016年 bilibili. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IJKMediaPlayer : NSObject
+
+@end
---
+++ VRPlayer-IOS/VRLive/IJKMediaPlayer/ios/IJKMediaPlayer/IJKMediaPlayer/IJKMediaPlayer.m
@@ -0,0 +1,13 @@
+//
+//  IJKMediaPlayer.m
+//  IJKMediaPlayer
+//
+//  Created by yangxiao on 16/6/1.
+//  Copyright © 2016年 bilibili. All rights reserved.
+//
+
+#import "IJKMediaPlayer.h"
+
+@implementation IJKMediaPlayer
+
+@end
